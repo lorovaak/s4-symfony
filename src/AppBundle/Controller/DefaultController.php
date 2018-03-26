a2eccd0e098365d8f74232ec8cb23a1dd4104e00
Laura Dietsch <laura.dietsch@etu.univ-grenoble-alpes.fr>
2018-03-26 06:40:07 +0000

    initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/cv/{error}", name="homepage")
+     * @Template()
+     */
+    public function indexAction($error = false)
+    {
+         $forms = $this->getDoctrine()->getRepository("AppBundle:Formation")->findAll();
+         $exps = $this->getDoctrine()->getRepository("AppBundle:Experience")->findAll();
+         $lois = $this->getDoctrine()->getRepository("AppBundle:Loisir")->findAll();
+        // // replace this example code with whatever you need
+         return array('name' => 'Dietsch', 'firstname' => "Laura", 'formations' => $forms, 'experiences' => $exps, 'loisirs' => $lois, 'error' => $error);
+        }
+    /**
+     * @Route("/admin", name="admin")
+     * @Template()
+     */
+    public function adminAction()
+    {
+        return array();
+    }
+}
